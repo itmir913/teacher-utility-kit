34f5fae6a96b07c239ea56eec3b00c4c2c700709
itmir913 <itmir913@gmail.com>
2026-03-27 20:34:40 +0900

    전문대 꿈꾸고 내보내기를 위한 커스텀 로직 추가

--- score-reporter/js/main.js
+++ score-reporter/js/main.js
@@ -49,20 +49,6 @@ function exceljsTo2DArray(ws) {
     return result;
 }
 
-// 영문 변수명을 한글 라벨로 변환해주는 매핑 사전
-const FIELD_LABELS = {
-    grade_year: '학년', class: '반', number: '번호', name: '이름',
-    kor_subject: '국어_선택과목명', kor_common_raw: '국어_공통원점수', kor_select_raw: '국어_선택원점수',
-    kor_raw: '국어_원점수', kor_std: '국어_표준점수', kor_pct: '국어_백분위', kor_grade: '국어_등급',
-    math_subject: '수학_선택과목명', math_common_raw: '수학_공통원점수', math_select_raw: '수학_선택원점수',
-    math_raw: '수학_원점수', math_std: '수학_표준점수', math_pct: '수학_백분위', math_grade: '수학_등급',
-    eng_raw: '영어_원점수', eng_std: '영어_표준점수', eng_pct: '영어_백분위', eng_grade: '영어_등급',
-    inq1_subject: '탐구1_과목명', inq1_raw: '탐구1_원점수', inq1_std: '탐구1_표준점수', inq1_pct: '탐구1_백분위', inq1_grade: '탐구1_등급',
-    inq2_subject: '탐구2_과목명', inq2_raw: '탐구2_원점수', inq2_std: '탐구2_표준점수', inq2_pct: '탐구2_백분위', inq2_grade: '탐구2_등급',
-    hist_raw: '한국사_원점수', hist_std: '한국사_표준점수', hist_pct: '한국사_백분위', hist_grade: '한국사_등급',
-    fl2_subject: '제2외국어_과목명', fl2_raw: '제2외국어_원점수', fl2_std: '제2외국어_표준점수', fl2_pct: '제2외국어_백분위', fl2_grade: '제2외국어_등급'
-};
-
 // 탭 전환 로직
 function switchTab(tabId) {
     ['upload', 'report', 'export'].forEach(t => {
